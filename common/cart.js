00565ec73d27c6620fae76e65ff3b08269fbce58
zhangbo <854008949@qq.com>
2019-12-04 23:24:25 +0800

    20191204

--- common/cart.js
+++ common/cart.js
@@ -42,9 +42,10 @@ const commitOrder = function(that){
 	var datetime = that.dateTimeValue;
 	var param = {"oPaymethod":that.selectValue,"oType":1,"o_remarks":that.textareaRemarks,"o_address":address,"oDeliverytime":datetime}
 	var odArray = [];
+	var cIdList = [];
 	for(var i = 0;i < that.cartProduct.length;i++){
 		var product = that.cartProduct[i];
-		var od = {"odPsid":product.cPsid,"odCount":product.cCount,"cTotal":product.cTotal}
+		var od = {"ocId":product.cId,"odPsid":product.cPsid,"odCount":product.cCount,"odTotal":product.cTotal}
 		odArray.push(od);
 	}
 	param.orderDetail = odArray;
@@ -53,19 +54,44 @@ const commitOrder = function(that){
 		console.log("提交订单:");
 		console.log(res);
 		var data = res.data.data;
-		var dataStr = encodeURIComponent(JSON.stringify(data))
+		var dataStr = JSON.stringify(data).replace("=","%3D");
 		var url = "/pages/cart/pay?payParam="+dataStr+"&productList="+JSON.stringify(that.cartProduct)+"&address="+address+"&datetime="+that.dateTimeLable+"&payMethod="+that.selectLabel+"&remarks="+that.textareaRemarks+"&zongjia="+that.zongjia;
 		uni.navigateTo({
 			url:url
-		})
-		
-		
+		});
 	})
+}
+
+
+const payOrder = function(that){
+	var pay = that.payParam
+	console.log(pay)
+	uni.requestPayment({
+	    provider: 'wxpay',
+	    timeStamp: pay.timeStamp,
+	    nonceStr: pay.nonceStr,
+	    package: pay.package,
+	    signType: pay.signType,
+	    paySign: pay.paySign,
+	    success: function (res) {
+	        console.log('success:' + JSON.stringify(res));
+			uni.navigateTo({
+				url:"/pages/cart/ok_Pay"
+			})
+	    },
+	    fail: function (err) {
+	        uni.navigateTo({
+	        	url:"/pages/cart/fail_Pay"
+	        })
+	    }
+	});
+	
 }
 
 export default{
 	selectCarts,
 	deleteCarts,
 	getDefaultAddress,
-	commitOrder
+	commitOrder,
+	payOrder
 }
