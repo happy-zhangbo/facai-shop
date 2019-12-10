99069fe70e6c712b43fb4d47ab75fa4c97eb77cc
zhangbo <854008949@qq.com>
2019-12-10 21:47:03 +0800

    20191210

---
+++ pages/my/about.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-black" :isBack="true">
+			<block slot="backText">返回</block>
+			<block slot="content">关于我们</block>
+		</cu-custom>
+		<view>
+			<view>售后支持：13811418229</view>
+			<view>技术支持：18010091016</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/my/customerService.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-black" :isBack="true">
+			<block slot="backText">返回</block>
+			<block slot="content">客服</block>
+		</cu-custom>
+		<view class="padding flex flex-direction" style="margin-top: 255px;">
+			<button class="cu-btn bg-black margin-tb-sm lg" open-type="contact">打开客服</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
