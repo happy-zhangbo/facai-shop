a9cc6928a0f554be79e277821231baaf97b10e84
zhangbo <854008949@qq.com>
2019-12-20 17:44:44 +0800

    20191220

--- pages/my/about.vue
+++ pages/my/about.vue
@@ -4,9 +4,9 @@
 			<block slot="backText">返回</block>
 			<block slot="content">关于我们</block>
 		</cu-custom>
-		<view>
-			<view>售后支持：13811418229</view>
-			<view>技术支持：18010091016</view>
+		<view class="text-center" style="margin-top: 100px;">
+			<view class="padding-tb">售后支持：13811418229</view>
+			<view class="padding-tb">技术支持：18010091016</view>
 		</view>
 	</view>
 </template>
